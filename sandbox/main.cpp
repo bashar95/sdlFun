55103eb39e15a9d6a2ff46016ece0831d5d8b029
wiseoldman95 <softwatt@gmx.com>
2014-09-24 20:58:29 +0300

    Added sandbox folder

---
+++ sandbox/main.cpp
@@ -0,0 +1,6 @@
+/*
+Feel free to fork and then play around with this file/folder, I will never modify it.
+The idea is that if I update the interface and the files at ../cpp or /c change, 
+no merge conflicts will occur if you merge my repo into your master as long as 
+you only play around with /sandbox.
+*/
