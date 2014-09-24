2cd2f1f0e6f6554ea797543920ecf688622cbde2
Wiseoldman95 <@>
2014-09-24 12:03:56 +0300

    Added c++, fixed makefile

--- cpp/compileAndRun.sh
+++
@@ -1,2 +0,0 @@
-g++ minimal.cpp sdl_helper.cpp -lSDL -lSDL_image -lm
-./a.out
---
+++ cpp/minimal.cpp
@@ -0,0 +1,75 @@
+//apt-get install libsdl-image1.2-dev libsdl-dev
+#include "sdl_helper.h"
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 600
+#define MAX_FPS 30
+
+
+int HandleInput();
+void GameLoop();
+void AnimateStuff();
+///////////////////////////////////////////////////
+
+int main(int argc, char *argv[])
+{
+    cSdlh::StartSDL(SCREEN_WIDTH,SCREEN_HEIGHT,MAX_FPS,"Minimal test",false);
+    GameLoop();
+    cSdlh::QuitSDL();
+    return 0;
+}
+
+void GameLoop()
+{
+	while (1)
+	{
+		AnimateStuff();
+		if (HandleInput()) return;
+		cSdlh::LimitFps();
+	}
+}
+
+int randomNumForFun=0;
+int deltaNumForFun=1;
+void AnimateStuff()
+{
+	cSdlh::DrawStart();
+	cSdlh::ClearScreen(); //Something interesting will happen if you comment this ;)
+	int x,y;
+	for (x=randomNumForFun; x<SCREEN_WIDTH; x++)
+	{
+	    for (y=0; y<SCREEN_HEIGHT; y++)
+	    {
+		cSdlh::DrawPixel(x,y,x%255,y%255,(x+y+randomNumForFun)%255);
+	    }
+	}
+	if (randomNumForFun>=SCREEN_WIDTH || randomNumForFun<0) deltaNumForFun*=-1;
+	randomNumForFun+=deltaNumForFun;
+	cSdlh::DrawEnd();
+}
+
+int HandleInput()
+{
+	SDL_Event event;
+	while ( SDL_PollEvent(&event) )
+	{
+	    switch (event.type)
+	    {
+			case SDL_MOUSEMOTION:
+				//random=random_function(event.motion.x,event.motion.y);
+			break;
+			
+			
+			//case SDL_MOUSEMOTION:
+			//case SDL_MOUSEBUTTONDOWN:
+				//event.button.x , event.button.y
+			//case SDL_KEYUP
+			case SDL_KEYDOWN:
+				//if (event.key.keysym.sym==SDLK_LEFT) blahblah
+				//if (event.key.keysym.sym==SDLK_LEFT) blahblah
+				//Works: SDLK_F10, SDLK_HOME, SDLk_ENTER, SDLK_w, SDLK_s, SDLK_1
+			case SDL_QUIT:
+				return 1;
+	    }
+	}
+}
+
