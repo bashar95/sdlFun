d032fca701cfb945cffc762560151d7728caa89e
wiseoldman95 <softwatt@gmx.com>
2014-09-24 21:01:13 +0300

    Added sandbox folder

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -3,4 +3,9 @@ Feel free to fork and then play around with this file/folder, I will never modif
 The idea is that if I update the interface and the files at ../cpp or /c change, 
 no merge conflicts will occur if you merge my repo into your master as long as 
 you only play around with /sandbox.
+
+
+If you want something to get you started, copy the contents of ../cpp/minimal.cpp here
+But change the #include line to:
+#include "../cpp/sdlfun.h"
 */
